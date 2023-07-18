fc85dad84c094e6fd49e5e0dcd4477cda42e7068
mishgan55 <vorota.mogilev@gmail.com>
2023-07-18 17:27:05 +0300

    Initial commit

---
+++ src/main/java/khorsun/springcourse/TestSpring.java
@@ -0,0 +1,18 @@
+package khorsun.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean = classPathXmlApplicationContext.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        classPathXmlApplicationContext.close();
+
+
+    }
+}
